91fe045e76365351b503db87b0d60bbbb9b21923
hanumansitaram <hanuman@unv.rs>
2021-03-15 08:59:50 +0100

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Site Thumbnail Getter")]
+[assembly: AssemblyDescription("The application that snaps a website screenshot and saves it to a file.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Adam Najmanowicz")]
+[assembly: AssemblyProduct("GetSiteThumbnail.Properties")]
+[assembly: AssemblyCopyright("2008 Adam Najmanowicz")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("203fc34d-f871-49fc-be94-bff0c84a0b12")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
